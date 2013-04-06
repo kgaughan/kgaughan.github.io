9e4bdd323200ea2184fe654faafc05c2e3e89db1
Keith Gaughan <k@stereochro.me>
2013-04-06 19:28:44 +0100

    First stab at the site.

---
+++ src/pelicanconf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Keith Gaughan'
+SITENAME = u'Stereochromy'
+SITEURL = 'http://kgaughan.github.io'
+
+TIMEZONE = 'Europe/Dublin'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS =  ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
